f259df858265865af0b28f6d3a6f2c9bf71a7366
tdhuynh <tommyhuynh93@gmail.com>
2016-09-21 16:23:24 -0400

    fixed tie condition

--- main.py
+++ main.py
@@ -17,23 +17,21 @@ def display(board):
     print("2  " + board[2][0] + "  |" + board[2][1] + "  |" + board[2][2])
 
 def game_start():
+    game_board = [[' ', ' ', ' '],
+                  [' ', ' ', ' '],
+                  [' ', ' ', ' ']]
+
     print("Player X will go first.\n")
     print("""Type in row then column of board with no space.
     '00' is top left, '12' is center right, etc.\n""")
     display(game_board)
     player_turn("X")
 
-def new_game():
-    game_board = [[' ', ' ', ' '],
-                  [' ', ' ', ' '],
-                  [' ', ' ', ' ']]
-    game_start()
-
 def player_turn(player):
     try:
         row, col = input("{}'s move: ".format(player))
         row, col = int(row), int(col)
-    except (IndexError, ValueError):
+    except IndexError:
         print("Try again. Row number then column number (00, 11, 22, etc.)")
         display(game_board)
         player_turn(player)
@@ -52,6 +50,13 @@ def next_player(player):
     else:
         player_turn("X")
 
+def has_spaces(game_board):
+    for row in game_board:
+        for i in row:
+            if i == " ":
+                return True
+    return False
+
 def win_condition(game_board, row, col, player):
     if player == game_board[row][0] == game_board[row][1] == game_board[row][2]:
         win_check(player)
@@ -61,10 +66,13 @@ def win_condition(game_board, row, col, player):
         win_check(player)
     if player == game_board[2][0] == game_board[1][1] == game_board[0][2]:
         win_check(player)
+    if has_spaces(game_board) == False:
+         print("It's a TIE!")
+         play_again()
 
 def win_check(player):
     print("{} wins!".format(player))
-    new_game()
+    play_again()
 
 def play_again():
     replay = input("Would you like to play again? [y/N] ")
