898bef8ed93b5aa9db10f5417afc0594d71b20f7
tdhuynh <tommyhuynh93@gmail.com>
2016-09-20 22:58:24 -0400

    redid board to a matrix

--- main.py
+++ main.py
@@ -8,19 +8,24 @@
 # print board with input
 # check for win, if no win, player x
 
+# game_board = {"00": " ", "01": " ", "02": " ",
+#               "10": " ", "11": " ", "12": " ",
+#               "20": " ", "21": " ", "22": " "}
 
-# board inspiration from book "Automate the Boring Stuff with Python" by Albert Sweigart
-game_board = {"00": " ", "01": " ", "02": " ",
-              "10": " ", "11": " ", "12": " ",
-              "20": " ", "21": " ", "22": " "}
+game_board = [[' ', ' ', ' '],
+              [' ', ' ', ' '],
+              [' ', ' ', ' ']]
+
+# display(board) modeled after printBoard(board) from Chapter 5 of
+#       book "Automate the Boring Stuff with Python" by Albert Sweigart.
 
 def display(board):
     print("   0    1    2 ")
-    print("0  " + board['00'] + "  |" + board['01'] + "  |" + board['02'])
+    print("0  " + board[0][0] + "  |" + board[0][1] + "  |" + board[0][2])
     print('   ---+---+---')
-    print("1  " + board['10'] + "  |" + board['11'] + "  |" + board['12'])
+    print("1  " + board[1][0] + "  |" + board[1][1] + "  |" + board[1][2])
     print('   ---+---+---')
-    print("2  " + board['20'] + "  |" + board['21'] + "  |" + board['22'])
+    print("2  " + board[2][0] + "  |" + board[2][1] + "  |" + board[2][2])
 
 display(game_board)
 
@@ -38,19 +43,24 @@ display(game_board)
 #     x_turn()
 
 def player_turn(player):
-    player_move = input("{}'s move -- pick a coordinate: ".format(player.upper()))
-    game_board[player_move] = "{}".format(player.upper())
-    display(game_board)
-    if player.upper() == "X":
-        player = "O"
-        return player_turn(player)
+    row, col = input("{}'s move: ".format(player))
+    row, col = int(row), int(col)
+    if game_board[row][col] == " ":
+        game_board[row][col] = "{}".format(player)
     else:
-        player = "X"
+        print("This coordinate has been taken. Choose another!")
         return player_turn(player)
 
+    display(game_board)
 
+    if player == "X":
+        return player_turn("O")
+    else:
+        return player_turn("X")
 
-print("Player X will go first.")
+print("Player X will go first.\n")
+print("""Type in row then column of board with no space.
+'00' is top left, '12' is center right, etc.\n""")
 
-while True:
+for turn in range(9):
     player_turn("X")
